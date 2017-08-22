94d848caceb4389dabea76a5d44bfa5941d1a096
kacper <kacper.kowalski@appunite.com>
2017-08-22 12:49:01 +0200

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Kacper Kowalski on 22.08.2017.
+//  Copyright © 2017 Kacper Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
