95331fff4ab43999346cb4c4d0313f4a9378b61b
kacper <kacper.kowalski@appunite.com>
2017-08-22 13:36:32 +0200

    created basic view cell setup

--- MovieList/MovieCell.swift
+++ MovieList/MovieCell.swift
@@ -10,14 +10,88 @@ import UIKit
 
 class MovieCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let movieImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 40
+        iv.layer.masksToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    let movieTitleLabel: UILabel = {
+        let l = UILabel()
+        l.sizeToFit()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let l = UILabel()
+        l.sizeToFit()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.numberOfLines = 0
+        return l
+    }()
+    
+    let dateLabel: UILabel = {
+        let l = UILabel()
+        l.sizeToFit()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.textAlignment = .right
+        l.numberOfLines = 0
+        return l
+    }()
+    
+    let reviewsCountLabel: UILabel = {
+        let l = UILabel()
+        l.sizeToFit()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }()
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpViews()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func setUpViews() {
+        
+        contentView.addSubview(movieImageView)
+        contentView.addSubview(movieTitleLabel)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(reviewsCountLabel)
+        
+        let marginGuide = contentView.layoutMarginsGuide
+        
+        NSLayoutConstraint.activate([
+            movieImageView.heightAnchor.constraint(equalToConstant: 80),
+            movieImageView.widthAnchor.constraint(equalToConstant: 80),
+            movieImageView.leftAnchor.constraint(equalTo: marginGuide.leftAnchor),
+            movieImageView.topAnchor.constraint(equalTo: marginGuide.topAnchor, constant: 20),
+            
+            movieTitleLabel.leftAnchor.constraint(equalTo: movieImageView.rightAnchor, constant: 15),
+            movieTitleLabel.centerYAnchor.constraint(equalTo: movieImageView.centerYAnchor),
+            
+            dateLabel.rightAnchor.constraint(equalTo: marginGuide.rightAnchor),
+            dateLabel.topAnchor.constraint(equalTo: marginGuide.topAnchor, constant: 15),
+            dateLabel.leftAnchor.constraint(equalTo: movieTitleLabel.leftAnchor),
+            
+            descriptionLabel.leftAnchor.constraint(equalTo: movieTitleLabel.leftAnchor),
+            descriptionLabel.rightAnchor.constraint(equalTo: marginGuide.rightAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: movieTitleLabel.bottomAnchor, constant: 15),
+            
+            reviewsCountLabel.leftAnchor.constraint(equalTo: movieTitleLabel.leftAnchor),
+            reviewsCountLabel.rightAnchor.constraint(equalTo: descriptionLabel.rightAnchor),
+            reviewsCountLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 15),
+            reviewsCountLabel.bottomAnchor.constraint(equalTo: marginGuide.bottomAnchor, constant: -15)
+            
+            ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
--- MovieList/ViewController.swift
+++ MovieList/ViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    static let cellIdentifier = "cellID"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        tableView.register(MovieCell.self, forCellReuseIdentifier: ViewController.cellIdentifier)
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
+        self.title = "MOVIES"
     
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ViewController.cellIdentifier, for: indexPath) as? MovieCell else {
+            fatalError()
+        }
+        
+        return cell
+    }
+    
 }
 
